fd8f1eaff7a505eee1e0c9f11f774cb6608300dc
Henrik Vasquez <motionbuilder@yahoo.com>
2024-01-22 13:24:34 -0700

    get new repo working

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace C_SharpLessons
+namespace Conditionals
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int theVal = 50;
+
+            if (theVal == 50) {
+                Console.WriteLine("theVal is 50");
+            }
+            else {
+                Console.WriteLine("theVal is something else");
+            }
         }
     }
 }
